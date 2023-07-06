426add186b3bed086fc13f0591b4a35aa2fa1f65
Tanjina <tanjinaproma@gmail.com>
2023-07-06 19:00:00 +0600

    Model trainer component  added

---
+++ src/ArticleSorting/components/model_trainer.py
@@ -0,0 +1,5 @@
+from transformers import TrainingArguments, Trainer
+from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_dataset, load_from_disk
+import torch
