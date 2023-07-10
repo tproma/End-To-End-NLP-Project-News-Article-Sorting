c94d1842cc31a237a5f00e649825459697496c8c
Tanjina <tanjinaproma@gmail.com>
2023-07-10 16:14:03 +0600

    data ingestion component modified

--- src/ArticleSorting/components/data_ingestion.py
+++ src/ArticleSorting/components/data_ingestion.py
@@ -5,3 +5,8 @@ from pathlib import Path
 from ArticleSorting.logging import logger
 from ArticleSorting.utils.common import get_size
 from ArticleSorting.entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
