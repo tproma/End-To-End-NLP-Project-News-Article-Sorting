6d82958d11db0c522b2d546b14c5768b82152a6c
Tanjina <tanjinaproma@gmail.com>
2023-10-23 19:28:38 +0600

    data ingestion pipeline modified

---
+++ src/ArticleSorting/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,5 @@
+from ArticleSorting.config.configuration import ConfigurationManager
+from ArticleSorting.components.data_ingestion import DataIngestion
+from ArticleSorting.logging import logger
+
+
