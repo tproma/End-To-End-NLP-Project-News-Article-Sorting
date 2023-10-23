3774776231abd7b6c28019142885e086716414c9
Tanjina <tanjinaproma@gmail.com>
2023-10-23 19:14:52 +0600

    zip file extraction code added

--- src/ArticleSorting/components/data_ingestion.py
+++ src/ArticleSorting/components/data_ingestion.py
@@ -21,4 +21,10 @@ class DataIngestion:
         else:
             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
           
-    
+    def extract_zip_file(self):
+        
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file,'r' ) as zip_ref:
+            zip_ref.extractall(unzip_path)
+
