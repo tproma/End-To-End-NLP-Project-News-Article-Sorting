0caebad14c3b6adaa8f45d82f6ba32436bfbef4f
Tanjina <tanjinaproma@gmail.com>
2023-10-24 11:44:27 +0600

    data validation pipeline added

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from ArticleSorting.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ArticleSorting.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ArticleSorting.logging import logger
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try: 
     logger.info(f"........stage {STAGE_NAME} started.......")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"........stage {STAGE_NAME} completed.......")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try: 
+    logger.info(f"........stage {STAGE_NAME} started.......")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"........stage {STAGE_NAME} completed.......")
 
 except Exception as e:
