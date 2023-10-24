79a0210f578ceb2c723c730684909b99b14f4629
Tanjina <tanjinaproma@gmail.com>
2023-10-24 11:29:02 +0600

    libs deleted

--- src/ArticleSorting/components/data_validation.py
+++ src/ArticleSorting/components/data_validation.py
@@ -1,6 +1,5 @@
 import os
 from ArticleSorting.logging import logger
-from datasets import load_dataset, load_from_disk
-from transformers import AutoTokenizer
 from ArticleSorting.entity import DataValidationConfig
+
 
