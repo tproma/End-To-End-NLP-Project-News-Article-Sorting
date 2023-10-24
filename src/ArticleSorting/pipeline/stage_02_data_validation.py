c57a07bb92d48ac6706b8e40aa23f0a990e41155
Tanjina <tanjinaproma@gmail.com>
2023-10-24 11:52:44 +0600

    data validation pipeline corrected

--- src/ArticleSorting/pipeline/stage_02_data_validation.py
+++ src/ArticleSorting/pipeline/stage_02_data_validation.py
@@ -8,7 +8,7 @@ class DataValidationTrainingPipeline():
 
     def main(self):
         config = ConfigurationManager()
-        data_validation_config = config.get_data_ingestion_config()
+        data_validation_config = config.get_data_validation_config()
         data_validation = DataValidation(config= data_validation_config)
         data_validation.validate_all_files_exist()
         
