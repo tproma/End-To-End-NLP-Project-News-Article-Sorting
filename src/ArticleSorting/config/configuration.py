eed5a906f2a4ba41d5e17438bcb2f289e2f633eb
Tanjina <tanjinaproma@gmail.com>
2023-10-24 11:15:14 +0600

    data validation config added"

--- src/ArticleSorting/config/configuration.py
+++ src/ArticleSorting/config/configuration.py
@@ -1,6 +1,8 @@
 from ArticleSorting.constants import *
 from ArticleSorting.utils.common import read_yaml, create_directories
-from ArticleSorting.entity import DataIngestionConfig
+from ArticleSorting.entity import (DataIngestionConfig,
+                                   DataValidationConfig,
+)
 
 class ConfigurationManager:
     def __init__(
@@ -27,3 +29,17 @@ class ConfigurationManager:
                 )
           
           return data_ingestion_config
+    
+
+    def get_data_validation_config(self) ->DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir= config.root_dir,
+            STATUS_FILE= config.STATUS_FILE,
+            ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
