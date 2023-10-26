90b531a3c20bbee988933cc570c2c3948b200115
Tanjina <tanjinaproma@gmail.com>
2023-10-27 00:22:12 +0600

    peipeline added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ArticleSorting.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ArticleSorting.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ArticleSorting.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from ArticleSorting.logging import logger
 
 
