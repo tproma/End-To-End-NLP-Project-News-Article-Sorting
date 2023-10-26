ef3fe10645cd5e13e816ea4caf18fc4f5b42519f
Tanjina <tanjinaproma@gmail.com>
2023-10-26 23:24:53 +0600

    data transformation entity added

--- src/ArticleSorting/entity/__init__.py
+++ src/ArticleSorting/entity/__init__.py
@@ -16,3 +16,9 @@ class DataValidationConfig:
     ALL_REQUIRED_FILES: list
 
 
+
+@dataclass(frozen = True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
