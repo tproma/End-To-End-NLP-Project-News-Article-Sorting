9ad74dc4f1866118207683203780cf0bafbc13b1
Tanjina <tanjinaproma@gmail.com>
2023-10-27 19:39:23 +0600

    data transformation pipeline added

--- main.py
+++ main.py
@@ -29,3 +29,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try: 
+    logger.info(f"........stage {STAGE_NAME} started.......")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f"........stage {STAGE_NAME} completed.......")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
