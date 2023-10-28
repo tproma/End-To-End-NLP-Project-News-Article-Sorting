14e08f22cf0b87309aa6d752bd3a2af8c7e1c06d
Tanjina <tanjinaproma@gmail.com>
2023-10-29 00:14:47 +0600

    model trainer configuration  added

--- src/ArticleSorting/config/configuration.py
+++ src/ArticleSorting/config/configuration.py
@@ -58,3 +58,29 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+
+
+    def get_model_trainer_config(self) -> ModeTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModeTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_ckpt = config.model_ckpt,
+            output_dir = params.output_dir,
+            learning_rate  = params.learning_rate,
+            per_device_train_batch_size  = params.per_device_train_batch_size,
+            per_device_eval_batch_size  = params.per_device_eval_batch_size,
+            num_train_epochs  = params.num_train_epochs,
+            weight_decay= params.weight_decay,
+            eval_steps= params.eval_steps,
+            evaluation_strategy= params.evaluation_strategy,
+            save_strategy = params.save_strategy,
+            load_best_model_at_end= params.load_best_model_at_end
+        )
+
+        return model_trainer_config
