5b1a4d08806e97150b56ba8a9e0ba7b120705a13
Tanjina <tanjinaproma@gmail.com>
2023-10-29 00:10:44 +0600

    model trainer entity added

--- src/ArticleSorting/entity/__init__.py
+++ src/ArticleSorting/entity/__init__.py
@@ -22,3 +22,22 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+
+
+@dataclass(frozen= True)
+class ModeTrainerConfig:
+  root_dir: Path
+  train_data_path: Path
+  test_data_path: Path
+  model_ckpt: Path
+  output_dir: Path
+  learning_rate: float
+  per_device_train_batch_size: int
+  per_device_eval_batch_size: int
+  num_train_epochs: int
+  weight_decay: float
+  eval_steps: int
+  evaluation_strategy: str
+  save_strategy: str
+  load_best_model_at_end: bool   
