baac7684e949a78dcd43c84303f39557689ddd39
Tanjina <tanjinaproma@gmail.com>
2023-10-29 00:19:00 +0600

    model trainer configuration  modified

--- src/ArticleSorting/config/configuration.py
+++ src/ArticleSorting/config/configuration.py
@@ -2,7 +2,8 @@ from ArticleSorting.constants import *
 from ArticleSorting.utils.common import read_yaml, create_directories
 from ArticleSorting.entity import (DataIngestionConfig,
                                    DataValidationConfig,
-                                   DataTransformationConfig
+                                   DataTransformationConfig,
+                                   ModeTrainerConfig
 )
 
 class ConfigurationManager:
