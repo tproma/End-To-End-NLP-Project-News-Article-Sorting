2f0df6129f8e62996731e248bba41c872c8af8f8
Tanjina <tanjinaproma@gmail.com>
2023-11-12 23:55:41 +0600

    keyword arguments deleted

--- src/ArticleSorting/pipeline/prediction.py
+++ src/ArticleSorting/pipeline/prediction.py
@@ -11,7 +11,7 @@ class PredictionPipeline:
     
     def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        
 
         pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
 
