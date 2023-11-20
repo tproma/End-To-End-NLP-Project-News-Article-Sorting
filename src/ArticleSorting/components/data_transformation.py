32677faac83a0bb45ba7bb50608785dc2500f1b1
Tanjina <tanjinaproma@gmail.com>
2023-11-21 00:31:10 +0600

    val data added in transformation config

--- src/ArticleSorting/components/data_transformation.py
+++ src/ArticleSorting/components/data_transformation.py
@@ -22,18 +22,28 @@ class DataTransformation:
         df = pd.read_csv(self.config.data_path)
         df['encoded_label'] = df['Category'].astype('category').cat.codes
 
-        ## Spliting the Data
+        '''Spliting the Data
         # Training dataset
-        train_data = df.sample(frac=0.8, random_state=42)
+        #train_data = df.sample(frac=0.8, random_state=42)
         # Testing dataset
-        test_data = df.drop(train_data.index)
+        #test_data = df.drop(train_data.index)
+        '''
+
+        # Split train , test and validatio dataset
+        np.random.seed(112)
+        df_train, df_test, df_val = np.split(df.sample(frac=1, random_state=35),
+                                     [int(0.7*len(df)), int(0.8*len(df))])
+
+        print(len(df_train), len(df_test), len(df_val)) 
+        
 
         # Convert pyhton dataframe to Hugging Face arrow dataset
-        hg_train_data = Dataset.from_pandas(train_data)
-        hg_test_data = Dataset.from_pandas(test_data)
-        print(hg_train_data, hg_test_data)
-        return hg_train_data, hg_test_data
-
+        hg_train_data = Dataset.from_pandas(df_train)
+        hg_test_data = Dataset.from_pandas(df_test)
+        hg_val_data = Dataset.from_pandas(df_val)
+        print(hg_train_data, hg_test_data, hg_val_data)
+        return hg_train_data, hg_test_data, hg_val_data
+   
 
 
     def tokenize_dataset(self, data):
