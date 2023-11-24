3d9fd51ed3079f9cd2d795b9ef7c57282ffa6d9d
Tanjina <tanjinaproma@gmail.com>
2023-11-24 22:31:38 +0600

    model evaluation config added in configuration mananger

--- src/ArticleSorting/config/configuration.py
+++ src/ArticleSorting/config/configuration.py
@@ -85,3 +85,21 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+      
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            test_data_path = config.test_data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+
+        )
+        return model_evaluation_config
+
