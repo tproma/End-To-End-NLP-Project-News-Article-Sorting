e02e0c5d73fdb136afcae7c643d14acce2d35ba5
Tanjina <tanjinaproma@gmail.com>
2023-11-25 22:23:24 +0600

    app.py created

---
+++ app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from ArticleSorting.pipeline.prediction import PredictionPipeline
+
