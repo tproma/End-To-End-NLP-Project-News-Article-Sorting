be11c346ef12695a0afb26ed7350912e1308a115
Tanjina <tanjinaproma@gmail.com>
2023-11-25 23:17:33 +0600

    training route

--- app.py
+++ app.py
@@ -16,3 +16,13 @@ app = FastAPI()
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
+
+
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
